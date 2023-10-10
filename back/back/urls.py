5fb44ffe489d258c2018850b019ec0574af8d23d
Leandro Sánchez <leanmsan@gmail.com>
2023-10-10 10:45:20 -0300

    Cambios en gitignore
    Signed-off-by: Leandro Sánchez <leanmsan@gmail.com>

--- back/back/urls.py
+++ back/back/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('insumo/', include('insumo.urls')),
+    #path('insumo/', include('insumo.urls')),
 ]
