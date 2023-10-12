5780a10029d9ea69af9d6ddf389e35c3c530ce14
Franco Azzetti <fazzetti@afantec.com>
2023-10-12 19:35:03 -0300

    agregado módulo de recetas

--- back/recetas/urls.py
+++ back/recetas/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
-from .views import RecetaView
+from .views import RecetaView, RecetadetalleView
 
 urlpatterns = [
     
     path('recetas/', RecetaView.as_view(), name="recetas_list"),
-    path('recetas/<int:id>/', RecetaView.as_view(), name="recetas_process")
+    path('recetas/<int:id>/', RecetaView.as_view(), name="recetas_process"),
+    path('receta_detalles/', RecetadetalleView.as_view(), name="recetadetalles_list"),
+    path('receta_detalles/<int:id>/', RecetadetalleView.as_view(), name="recetadetalles_process"),
+    path('recetaxprod/<int:prod>/', RecetadetalleView.as_view(), name="recetaxprod")
     
 ]
