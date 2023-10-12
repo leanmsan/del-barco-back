2ff47f6533f2a1c78634879cd6c7a4a9b5f287f0
Facundo Paz Buxeda <95000006+MrTrebuchet@users.noreply.github.com>
2023-10-12 18:08:04 -0300

    arreglado

--- back/recetas/urls.py
+++ back/recetas/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from .views import ProveedorView
+from .views import RecetaView
 
 urlpatterns = [
     
-    path('recetas/', ProveedorView.as_view(), name="recetas_list"),
-    path('recetas/<int:id>/', ProveedorView.as_view(), name="recetas_process")
+    path('recetas/', RecetaView.as_view(), name="recetas_list"),
+    path('recetas/<int:id>/', RecetaView.as_view(), name="recetas_process")
     
 ]
