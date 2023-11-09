77506b9d1138cf6e49d48a324a91a9d91d701095
Leandro Sánchez <103838718+leanmsan@users.noreply.github.com>
2023-11-09 15:22:52 -0300

    Correccion de urls lastId

--- back/movimientos/urls.py
+++ back/movimientos/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('entradas/<int:id>/', EntradaView.as_view(), name="entradas_process"),
     path('entradasxprov/<int:prov>/', EntradaView.as_view(), name="entradasxprov"),
 
-    path('lastidEntrada/', GetLastIdEntradaView.as_view(), name="getlastid"),
-    path('lastidSalida/', GetLastIdSalidaView.as_view(), name="getlastid"),
+    path('lastidentrada/', GetLastIdEntradaView.as_view(), name="getlastid"),
+    path('lastidsalida/', GetLastIdSalidaView.as_view(), name="getlastid"),
 
     path('entrada_detalles/', EntradadetalleView.as_view(), name="entradadetalles_list"),
     path('entrada_detalles/<int:id>/', EntradadetalleView.as_view(), name="entradadetalles_process"),
